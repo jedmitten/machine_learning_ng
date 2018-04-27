051404594fbadcc13d9b51f13cbd24d0cef576dc
Jed Mitten <jedmitten@gmail.com>
2018-04-27 01:29:08 -0700

    finished computeCost and gradientDescent for class

--- assignments/machine-learning-ex1/ex1/gradientDescent.m
+++ assignments/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,12 +16,11 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
+    % implement gradient descent from lecture 05 slides w/ 
+    % mentor matrix multiplication hints from computeCost
+    h = X * theta;
+    values = X' * (h - y);
+    theta = theta - (alpha * (1 / m)) * values;
 
     % ============================================================
 
