eeca1b3366182654db92ee0c0174df348ce97d19
Lughead <jedmitten@alumni.cmu.edu>
2018-06-30 12:05:51 -0700

    finished ex7 (data recovery)

--- assignments/ex7/recoverData.m
+++ assignments/ex7/recoverData.m
@@ -21,6 +21,9 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %               Notice that U(j, 1:K) is a row vector.
 %               
 
+% Z is m x k
+% U is n x n, but use only the top K eigenvectors
+X_rec = Z * U(:, 1:K)';
 
 
 % =============================================================
